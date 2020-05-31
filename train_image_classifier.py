cc2463765674d9c1c8833f7f0fcd887bf38489e5
sudormroot <sudormroot@163.com>
2020-05-31 13:18:50 +0800

    add

--- train_image_classifier.py
+++ train_image_classifier.py
@@ -587,7 +587,7 @@ def main(_):
         save_summaries_secs=FLAGS.save_summaries_secs,
         save_interval_secs=FLAGS.save_interval_secs,
         sync_optimizer=optimizer if FLAGS.sync_replicas else None,
-        hooks)
+        hooks=hooks)
 
 
 if __name__ == '__main__':
